a47fc9ca0be1661dcb5479189156a1b6a1cf9b56
Caio Machado <moliveiracaio@gmail.com>
2024-11-13 16:04:08 -0300

    fixing bugs

--- main.py
+++ main.py
@@ -3,88 +3,69 @@ from dotenv import load_dotenv
 import os
 from typing import List
 
-
-
+# Load environment variables from .env file
 load_dotenv()
 
-# Configurações da AWS a partir do .env
-AWS_ACCESS_KEY_ID: str = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY: str = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION: str = os.getenv('AWS_REGION')
-BUCKET_NAME: str = os.getenv('BUCKET_NAME')
+# AWS Configurations from .env
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('AWS_REGION')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
 
-
+# Configure S3 Client
 s3_client = boto3.client(
     's3',
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     region_name=AWS_REGION
 )
+print("S3 client configured successfully.")
 
-# Configura o cliente S3
-try:
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name=AWS_REGION
-    )
-    print("Client configuration succeed.")
-except Exception as e:
-    print(f"Error configuring client: {e}")
-    raise
-
-def list_files(folder:str) -> List[str]:
-    files: List[str] = []
+def list_files(folder: str) -> List[str]:
+    files = []
     try:
         for file_name in os.listdir(folder):
-            path_complet = os.path.join(folder, file_name)
-            if os.path.isfile(path_complet):
-                files.append(path_complet)
-        print(f"Files listed on folder'{folder}: {files}")
+            path_complete = os.path.join(folder, file_name)
+            if os.path.isfile(path_complete):
+                files.append(path_complete)
+        print(f"Files listed in folder '{folder}': {files}")
     except Exception as e:
-        print(f"Error to list files on folder '{folder}': {e}")
+        print(f"Error listing files in folder '{folder}': {e}")
         raise
     return files
 
-
 def upload_files_to_s3(files: List[str]) -> None:
     for file in files:
-        file_name: str = os.path.basename(file)
+        file_name = os.path.basename(file)
         try:
+            print(f"Attempting to upload '{file_name}' to bucket '{BUCKET_NAME}'...")
             s3_client.upload_file(file, BUCKET_NAME, file_name)
-            print(f'{file_name} sent to S3.')
+            print(f"'{file_name}' successfully uploaded to S3.")
         except Exception as e:
-            print(f"Error on sending '{file_name}' to S3.")
-            raise
-        
+            print(f"Error uploading '{file_name}' to S3: {e}")
 
-def delete_local_file(files: List[str]) -> None:
+def delete_local_files(files: List[str]) -> None:
     for file in files:
         try:
             os.remove(file)
-            print(f'{file} was deleted from local folder.')
+            print(f"'{file}' was deleted from the local folder.")
         except Exception as e:
-            print(f"Error on deleting file '{file}' :{e}")
-            raise
-
+            print(f"Error deleting file '{file}': {e}")
 
 def execute_backup(folder: str) -> None:
     try:
-        files: List[str] = list_files(folder)
+        files = list_files(folder)
         if files:
             upload_files_to_s3(files)
-            delete_local_file(files)
-
+            delete_local_files(files)
         else:
-            print("No file found for backup.")
+            print("No files found for backup.")
     except Exception as e:
-        print(f"Error on processing backup: {e}")
+        print(f"Error processing backup: {e}")
         raise
 
-
 if __name__ == '__main__':
-    LOCAL_FOLDER: str = 'download'
+    LOCAL_FOLDER = 'download'
     try:
         execute_backup(LOCAL_FOLDER)
     except Exception as e:
